32a78dd378a1df2522ee22051ccdcc5d58ab6c5f
chrismelyan <chris.shelpuk@gmail.com>
2022-02-10 20:39:44 +0300

    homework 1

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Message, {MessageType} from "./Message";
+
+const messageData: MessageType = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Chris',
+    message: 'Yeah! Nailed it!!!',
+    time: '22:00'
+}
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            should work (должно работать)
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import classes from './Message.module.css'
+
+export type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageType) {
+    return (
+        <div className={classes.container}>
+            <img src={props.avatar} alt={'avatar'}/>
+            <div className={classes.bubble}>
+                <div className={classes.name}>{props.name}</div>
+                <div className={classes.message}>{props.message}</div>
+                <div className={classes.time}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
